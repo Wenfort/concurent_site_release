a9cb220f4babaea5e7db2e665e8ae00f0c67e14c
notebook_linux <misteriska@ya.ru>
2021-01-05 19:05:58 +0200

    выключил дебаг и добавил домен в разрешенные

--- concurent_site/settings.py
+++ concurent_site/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'zs_8uh!va$xehk*ld=8s&1-%a1bc8)gdgt6t*9-%7tuk3gg+i1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'seonior.ru']
 
 
 # Application definition
