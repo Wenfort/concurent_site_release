6ede0a6c15de59eeeb09b39061f787d98d022c15
notebook_linux <misteriska@ya.ru>
2021-02-03 08:07:07 +0300

    готов вывод всех заказов

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('results', views.results, name='publish_results'),
     path('add', views.add_new_requests_to_queue, name='add_new_requests_to_queue'),
+    path('orders', views.get_orders_page, name='get_orders_page'),
 ]
--- main/views.py
+++ main/views.py
@@ -23,13 +23,15 @@ def results(request):
     else:
         return render(request, 'main/non_restricted_requests.html', context)
 
+def get_user_data(request):
+    user_name = request.user.username
+    user = UserData.objects.get(name=user_name)
+    return user
 
 def add_new_requests_to_queue(request):
     if request.method == "POST":
 
-        user_name = request.user.username
-        user = UserData.objects.get(name='airlove')
-
+        user_data = get_user_data(request)
 
         requests_list = request.POST['requests_list']
         requests_list = requests_list.replace('\r', '')
@@ -41,14 +43,23 @@ def add_new_requests_to_queue(request):
                 RequestQueue(request=req).save()
                 Request(request=req).save()
 
-        # order_id = Order.objects.latest('pk').id + 1
-        order_id = 1
+        order_id = Order.objects.latest('pk').id + 1
 
         new_requests = Request.objects.filter(request__in=requests_list)
         for new_request in new_requests:
-            Order(request_id=new_request.id, user_id=user.id, order_id = order_id).save()
+            Order(request_id=new_request.id, user_id=user_data.id, order_id = order_id).save()
 
         return HttpResponseRedirect('/main/results')
     else:
         form = NewRequest()
         return render(request, 'main/add_to_queue.html', {'form': form})
+
+def get_orders_page(request):
+    user_data = get_user_data(request)
+    user_id = user_data.id
+
+    orders = Order.objects.distinct('order_id').filter(user_id=user_id)
+    orders_amount = len(orders)
+
+    context = {'orders': orders, 'user': user_data, 'orders_amount': orders_amount}
+    return render(request, 'main/orders.html', context)
