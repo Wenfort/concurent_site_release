f195c101c1b1b31ba532ceb9e10f533a739e1312
notebook_linux <misteriska@ya.ru>
2021-02-04 14:39:09 +0300

    add работаю на каждой странице

--- main/urls.py
+++ main/urls.py
@@ -5,8 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('results', views.results, name='results'),
-    path('add', views.add_new_requests_to_queue, name='add_new_requests_to_queue'),
     path('orders', views.get_orders_page, name='get_orders_page'),
     path('balance', views.balance, name='balance'),
-    path('ordered_requests', views.ordered_requests, name='ordered_requests')
 ]
--- main/views.py
+++ main/views.py
@@ -9,7 +9,6 @@ from .forms import NewRequest
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
-
 class User:
     def __init__(self, name):
         self.name = name
@@ -20,7 +19,6 @@ class User:
         self.unique_order_rows = object
         self.ordered_keywords_amount = int
         self.orders_amount = int
-
 
         self.get_user_data()
         self.get_user_id()
@@ -48,76 +46,101 @@ class User:
         self.ordered_keywords_amount = len(self.all_order_rows)
         self.orders_amount = len(self.unique_order_rows)
 
+class NewRequestHandler:
+    def __init__(self, request):
+        self.request = request.POST
+        self.user_name = request.user.username
+        self.user_data = object
+        self.requests_list = list()
+        self.order_id = int()
+        self.new_requests = list()
+        self.new_requests_amount = int()
 
-def results(request):
-    all_requests_list = get_list_or_404(Request.objects.order_by('site_seo_concurency'))
-    user_data = User(request.user.username)
+        self.get_user_data()
+        self.make_requests_list()
+        self.add_new_requests_to_database()
+        self.get_order_id()
+        self.get_new_requests_id()
+        self.calculate_new_requests_amount()
+        self.update_user_orders()
+        self.update_user_balance()
 
-    try:
-        context = {'all_requests_list': all_requests_list,
-                   'orders': user_data.orders_amount,
-                   'keywords_ordered': user_data.ordered_keywords_amount,
-                   'balance': user_data.balance}
-    except:
-        context = {'all_requests_list': all_requests_list}
-    if request.user.is_staff:
-        return render(request, 'main/restricted_requests.html', context)
-    else:
-        return render(request, 'main/non_restricted_requests.html', context)
+    def get_user_data(self):
+        self.user_data = User(self.user_name)
 
-
-def add_new_requests_to_queue(request):
-    if request.method == "POST":
-
-        user_data = User(request.user.username)
-
-        requests_list = request.POST['requests_list']
+    def make_requests_list(self):
+        requests_list = self.request['requests_list']
         requests_list = requests_list.replace('\r', '')
         requests_list = requests_list.split('\n')
-        for req in requests_list:
-            if Request.objects.filter(request=req):
+        self.requests_list = requests_list
+
+    def add_new_requests_to_database(self):
+        for request in self.requests_list:
+            if Request.objects.filter(request=request):
                 pass
             else:
-                RequestQueue(request=req).save()
-                Request(request=req).save()
+                RequestQueue(request=request).save()
+                Request(request=request).save()
 
-        order_id = Order.objects.latest('pk').id + 1
+    def get_order_id(self):
+        self.order_id = Order.objects.latest('pk').id + 1
 
-        new_requests = Request.objects.filter(request__in=requests_list)
-        new_requests_amount = len(new_requests)
-        for new_request in new_requests:
+    def get_new_requests_id(self):
+        self.new_requests = Request.objects.filter(request__in=self.requests_list)
+
+    def calculate_new_requests_amount(self):
+        self.new_requests_amount = len(self.new_requests)
+
+    def update_user_orders(self):
+        for new_request in self.new_requests:
             Order(request_id=new_request.id,
-                  user_id=user_data.id,
-                  order_id=order_id
+                  user_id=self.user_data.id,
+                  order_id=self.order_id
                   ).save()
 
-        UserData(id=user_data.id,
-                 balance=user_data.balance - new_requests_amount,
-                 name=user_data.name,
+    def update_user_balance(self):
+        UserData(id=self.user_data.id,
+                 balance=self.user_data.balance - self.new_requests_amount,
+                 name=self.user_data.name,
                  ).save()
 
+def results(request):
+
+    if request.method == "POST":
+        NewRequestHandler(request)
         return HttpResponseRedirect('/main/results')
     else:
-        form = NewRequest()
-        return render(request, 'main/add_to_queue.html', {'form': form})
+        user_data = User(request.user.username)
+        all_requests_list = get_list_or_404(Request.objects.order_by('site_seo_concurency'))
+        try:
+            context = {'all_requests_list': all_requests_list,
+                       'orders': user_data.orders_amount,
+                       'keywords_ordered': user_data.ordered_keywords_amount,
+                       'balance': user_data.balance}
+        except:
+            context = {'all_requests_list': all_requests_list}
+        if request.user.is_staff:
+            return render(request, 'main/restricted_requests.html', context)
+        else:
+            return render(request, 'main/non_restricted_requests.html', context)
 
 
 def get_orders_page(request):
-    user_data = User(request.user.username)
+    if request.method == "POST":
+        NewRequestHandler(request)
+        return HttpResponseRedirect('/main/orders')
+    else:
+        user_data = User(request.user.username)
+        form = NewRequest()
 
-    form = NewRequest()
-    context = {'all_orders_list': user_data.unique_order_rows,
-               'orders': user_data.orders_amount,
-               'keywords_ordered': user_data.ordered_keywords_amount,
-               'balance': user_data.balance,
-               'form': form}
+        context = {'all_orders_list': user_data.unique_order_rows,
+                   'orders': user_data.orders_amount,
+                   'keywords_ordered': user_data.ordered_keywords_amount,
+                   'balance': user_data.balance,
+                   'form': form}
 
-    return render(request, 'main/orders.html', context)
+        return render(request, 'main/orders.html', context)
 
 
 def balance(request):
-    pass
-
-
-def ordered_requests(request):
     pass
