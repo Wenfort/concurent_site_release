5bfdaf9d4ce666112ce3fd16051cd449fd1f8e8d
notebook_linux <misteriska@ya.ru>
2021-06-12 15:02:17 +0300

    Оптимизация кода user_logic.py

--- main/user_logic/user_logic.py
+++ main/user_logic/user_logic.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.core.mail import send_mail
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout as django_logout
-from main.models import UserData, Region
 from django.contrib.auth.models import User
+
+from main.models import UserData, Region
 from main.tools.password_generator import generate_password
+from main.tools.microservice_email_sender import MailEngine
 
 from django.contrib import messages
 
@@ -17,39 +19,40 @@ class SiteUser:
         self.orders = int
         self.ordered_keywords = int
         self.region_id = int
-        self.region = str
+        self.region_name = str
 
-        self.get_data()
-        self.get_balance()
-        self.get_orders()
-        self.get_ordered_requests()
-        self.get_region()
+        self.__get_user_data()
 
-    def get_data(self):
+    def __get_user_data(self):
         self.user_billing = UserData.objects.filter(user_id=self.id).select_related()
         self.user_billing = self.user_billing[0]
 
-    def get_balance(self):
+        self._get_balance()
+        self._get_orders()
+        self._get_ordered_requests()
+        self._get_region_name()
+
+    def _get_balance(self):
         self.balance = self.user_billing.balance
 
-    def get_orders(self):
+    def _get_orders(self):
         self.orders = self.user_billing.orders_amount
 
-    def get_ordered_requests(self):
+    def _get_ordered_requests(self):
         self.ordered_keywords = self.user_billing.ordered_keywords
 
-    def get_region(self):
+    def _get_region_name(self):
         self.region_id = self.user_billing.region_id
-        self.region = Region.objects.get(id=self.region_id).name
+        self.region_name = Region.objects.get(id=self.region_id).name
 
 
 class NewUserHandler:
     def __init__(self, request):
         self.request = request.POST
-        self.user_name = str
-        self.password = str
-        self.password_again = str
-        self.email = str
+        self.user_name = str()
+        self.password = str()
+        self.password_again = str()
+        self.email = str()
         self.valid = True
         self.status_messages = list()
 
@@ -76,11 +79,27 @@ class NewUserHandler:
     def get_user_email(self):
         self.email = self.request['email'].lower()
 
+        if '@' not in self.email and '.' not in self.email:
+            self.valid = False
+            self.status_messages.append('Пожалуйста, укажите корректный e-mail')
+
     def check_username_and_password(self):
         all_users = User.objects.all()
 
         login_is_busy = all_users.filter(username=self.user_name)
         email_is_busy = all_users.filter(email=self.email)
+
+        is_login_length_enough = 6 <= len(self.user_name) <= 30
+
+        if not is_login_length_enough:
+            self.valid = False
+            self.status_messages.append('Имя пользователя должно быть от 6 до 30 символов')
+
+        is_password_length_enough = 6 <= len(self.password) <= 30
+
+        if not is_password_length_enough:
+            self.valid = False
+            self.status_messages.append('Длина пароля должна быть от 6 до 30 символов')
 
         if login_is_busy:
             self.valid = False
@@ -99,14 +118,24 @@ class NewUserHandler:
     def compare_passwords(self):
         return self.password == self.password_again
 
-
+@login_required
 def change_password(request):
 
     if request.method == "POST":
         post_request = request.POST
+
+
         if post_request['first_password'] == post_request['second_password']:
+            is_password_length_enough = 6 <= len(post_request['first_password']) <= 30
+
+            if not is_password_length_enough:
+                message = 'Длина пароля должна быть от 6 до 30 символов'
+                context = {
+                    'message': message
+                }
+                return render(request, 'main/user_auth/change_password.html', context)
             password = post_request['first_password']
-            username = request.account_data.username
+            username = request.user.username
             user = User.objects.get(username=username)
             user.set_password(password)
             user.save()
@@ -127,23 +156,18 @@ def change_password(request):
         return render(request, 'main/user_auth/change_password.html')
 
 
+@login_required
 def password_reset(request):
-    if request.account_data.is_authenticated:
-        return HttpResponse('Вы уже авторизировались')
     if request.method == "POST":
-
-        post_request = request.POST
-        recipients = post_request['email'].lower(),
+        mail_engine = MailEngine(request.POST)
 
         try:
-            user = User.objects.get(email=recipients[0])
+            user = User.objects.get(email=mail_engine.recipients[0])
             password = generate_password()
             user.set_password(password)
             user.save()
 
-            send_mail('Письмо с паролем',
-                      f'Ваш временный пароль: {password}. Пожалуйста, смените его сразу после авторизации. Ссылка для авторизации: https://seonior.ru/authorization',
-                      'admin@seonior.ru', recipients)
+            mail_engine.send_reset_password_mail_via_django(password)
             message = 'Новый пароль отправлен на указанный e-mail. Пожалуйста, проверьте папку "Спам"'
         except:
             message = 'Пользователь с таким e-mail не найден'
@@ -157,6 +181,7 @@ def password_reset(request):
         return render(request, 'main/user_auth/password_reset.html')
 
 
+@login_required
 def balance(request):
     user_data = SiteUser(request.user)
     context = {'orders': user_data.orders,
@@ -169,67 +194,63 @@ def balance(request):
 def registration(request):
     if request.user.is_authenticated:
         return HttpResponse('Вы уже зарегистрированы')
-    else:
-        if request.method == "POST":
-            user = NewUserHandler(request)
-            if user.valid:
-                user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
-                login(request, user)
-                return HttpResponseRedirect('/orders')
-            else:
-                context = {
-                    'errors_list': user.status_messages,
-                    'status': 'retry',
-                    'username': request.POST['username'],
-                    'email': request.POST['email']
-                }
-                return render(request, 'main/user_auth/registration.html', context)
+
+    if request.method == "POST":
+        user = NewUserHandler(request)
+        if user.valid:
+            user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+            login(request, user)
+            return HttpResponseRedirect('/orders')
         else:
             context = {
+                'errors_list': user.status_messages,
+                'status': 'retry',
+                'username': request.POST['username'],
+                'email': request.POST['email']
             }
             return render(request, 'main/user_auth/registration.html', context)
+    else:
+        return render(request, 'main/user_auth/registration.html')
 
 
 def authorization(request):
     if request.user.is_authenticated:
         return HttpResponse('Вы уже авторизировались')
-    else:
-        if request.method == "POST":
-            user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
-            if user:
-                login(request, user)
-                try:
-                    return HttpResponseRedirect(request.GET['next'])
-                except:
-                    return HttpResponseRedirect('/results')
-            else:
-                context = {
-                    'error': 'Логин и пароль не совпадают'
-                }
 
-                return render(request, 'main/user_auth/authorization.html', context)
+    if request.method == "POST":
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user:
+            login(request, user)
+            return HttpResponseRedirect('/results')
         else:
             context = {
+                'error': 'Логин и пароль не совпадают'
             }
 
             return render(request, 'main/user_auth/authorization.html', context)
+    else:
+        return render(request, 'main/user_auth/authorization.html')
 
 
 def change_region(request):
-    user_id = request.account_data.id
+    user_id = request.user.id
     new_region = request.POST['region']
     previous_url = request.POST['previous_url']
     if new_region:
-        new_region_id = Region.objects.get(name=new_region).region_id
-        user = UserData.objects.filter(user_id=user_id)
-        user.update(region_id=new_region_id)
-        messages.success(request, 'Регион успешно изменен')
+        try:
+            new_region_id = Region.objects.get(name=new_region).id
+            user = UserData.objects.filter(user_id=user_id)
+            user.update(region_id=new_region_id)
+            messages.success(request, 'Регион успешно изменен')
+        except:
+            messages.error(request, f'К сожалению, регион {new_region} не поддерживается')
     else:
         messages.error(request, 'Поле "регион" не может быть пустым')
 
     return HttpResponseRedirect(previous_url)
 
 
+@login_required
 def logout(request):
     django_logout(request)
     return HttpResponseRedirect('/authorization')
