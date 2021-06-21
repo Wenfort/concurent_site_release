523a7ff9245836c705ceb6c81c9232d8c923b9f4
notebook_linux <misteriska@ya.ru>
2021-06-21 21:34:44 +0300

    WEIGHTS перенесены в переменные окружения

--- microservices/conc_settings.py
+++ microservices/conc_settings.py
@@ -4,111 +4,11 @@ CORE_NUMBER = os.environ['CORE_NUMBER']
 REQUEST_COST = os.environ['REQUEST_COST']
 CURRENT_YEAR = os.environ['CURRENT_YEAR']
 
-HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                  "Chrome/84.0.4147.135 Safari/537.36",
-    'accept': '*/*',
-    'accept-language': 'ru,en-US;q=0.9,en;q=0.8',
-    'cookie': 'mda=0; gdpr=0; _ym_uid=1591953493785318608; font_loaded=YSv1; my=YwA=; '
-              'fuid01=5f0083ed25741dd5.C4i0vqjwe3scLCrPaN6bv1ukRcqzdzhNW1hlM4'
-              '-5iFE7rGJ3VVU4OfYHY8nSPUAlnhWSl0Hgz3Z4bIJ9bBdJk6erpOWeLFasMkZPrLyi6BXY5aeR4JGJUkgGy1uO5tjX; '
-              'ymex=1911473885.yrts.1596113885#1911473885.yrtsi.1596113885; yandexuid=5424663781591953330; '
-              'yuidss=5424663781591953330; '
-              'zm=m-white_bender_zen-ssr.webp.css-https%3As3home-static_hvn1shYTGNNIEp9PevTjN_q8Lvc%3Al; '
-              'yandex_gid=8; i=rWPfumq04NP/XvqoGjACc26KaNCl72gtD18U2K8QQrnU6k1r3qTlDBMcJgwoRG8OV1FVOUGh'
-              '/ELcZ4PFrluvNBFIN9c=; Session_id=3:1598135941.5.0.1598135941683:ku3CsA:56.1|12955710.0.2|221821'
-              '.693338.JP2L2Qx2Ue6M1iB6ZiKWayY57BE; '
-              'sessionid2=3:1598135941.5.0.1598135941683:ku3CsA:56.1|12955710.0.2|221821.46184.wzIJs'
-              '-ywzudpu7ZIGgNPVN3ajO4; L=YU9pXgl0T0V3TQREfmRWVw9RA0RdDWULeAwmN1QbXxkxWQ==.1598135941.14335.355831'
-              '.bc31493b24a3e9f4e14fe2254ca38b49; yandex_login=Misteriska; _ym_isad=2; '
-              'yc=1598435472.zen.cacS%3A1598179872; _ym_d=1598177592; '
-              'yabs-frequency=/5/0G0207MJGLyPmpTV'
-              '/yeVSRv40002yF27dXznlaG000Bmy8Kffi72L0000l3mXiujoS9G0002yF25R_N9mb0000Bmy8K1si72L0000l3n1m_5oS9G0002yF25VTh1mbG000Bmy8SJaSd2K0000l3nXFOboS9G0002yF24NZ79mb0000BqymG4__t9mb0000Bmy8KnKi72L0000l3mXkdQmS9K0002yF20KLh1mbG000BmyOFPki72L0000l3mWzMwmS9K0002yF43xLB1mbG000BqyG06sXN9mb0000BmyG8bli72L0000l3n02NImS9K0002yF81JSB1mbG000Bqyu05GSB1mbG000BmyG0PKi72L0000lJnW0UjKi72L0000lJm00UnKi72L0000lJo00WLKi72L0000lJn00nbqi7000000l3mWfs-mS000002yF83MMB1m00000Bmy81Xji7000000l3mW95QmS000002yF21xLR1m00000Bmy80zqi7000000l3mWLN2mS000002yF20ETB1m00000BmyO6fki7000000l3mW6dImS000002yFC1-LR1m00000BmyG0Pki7000000l3p006wmS000002yF800/; _ym_visorc_50377519=b; _ym_visorc_722818=b; _ym_visorc_93511=w; cycada=PiE7o/p+4R8/ahWLksVcTD1Oh+0rna54bK+5NqJa/fQ=; ys=svt.1#wprid.1598179162889295-696864599197212741000179-production-app-host-vla-web-yp-221; yp=1613905110.szm.1%3A1920x1080%3A1920x920#1600814558.csc.2#1629715162.p_sw.1598179162#1908549868.sad.1593101818%3A1593189868%3A2#1909756795.multib.1#1627169731.stltp.serp_bk-map_1_1595633731#1600552505.ygu.1#1598219412.ln_tp.01#1598738958.zlgn_smrt.1#1913495941.udn.cDpNaXN0ZXJpc2th',
-    'origin': 'https://yastatic.net',
-    'referer': 'https://yastatic.net/safeframe-bundles/0.69/1-1-0/render.html',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'cross-site',
-}
-REGIONS = {
-    'Курск': '8',
-    'Москва': '213',
-}
-WEIGHTS_ORGANIC = {
-    1: 33,
-    2: 22,
-    3: 17,
-    4: 12,
-    5: 9,
-    6: 6,
-    7: 4,
-    8: 3,
-    9: 3,
-    10: 2,
-    11: 2,
-    12: 2,
-    13: 2,
-    14: 2,
-    15: 2,
-    16: 2,
-    17: 2,
-    18: 2,
-    19: 2,
-    20: 2,
-    21: 2,
-    22: 2,
-    23: 2,
-    24: 2,
-    25: 2,
-    26: 2,
-    27: 2,
-    28: 2,
-    29: 2,
-    30: 2,
-    31: 2,
-    32: 2,
-    33: 2,
-    34: 2,
-    35: 2,
-}
+HEADERS = os.environ['HEADERS']
 
-WEIGHTS_DIRECT = {
-    1: 21,
-    2: 15,
-    3: 11,
-    4: 8,
-    5: 6,
-    6: 4,
-    7: 3,
-    8: 2,
-    9: 2,
-    10: 2,
-    11: 2,
-    12: 2,
-    13: 2,
-    14: 2,
-    15: 2,
-    16: 2,
-    17: 2,
-    18: 2,
-    19: 2,
-    20: 2,
-    21: 2,
-    22: 2,
-    23: 2,
-    24: 2,
-    25: 2,
-    26: 2,
-    27: 2,
-    28: 2,
-    29: 2,
-    30: 2,
-    31: 2,
-    32: 2,
-    33: 2,
-    34: 2,
-    35: 2,
-}
+WEIGHTS_ORGANIC = os.environ['WEIGHTS_ORGANIC']
+
+WEIGHTS_DIRECT = os.environ['WEIGHTS_DIRECT']
 
 ABSURD_STEM_BREAKPOINT = 30
 
